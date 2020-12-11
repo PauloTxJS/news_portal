c583a449b4d75d79e7ca4842cf5377d14a9b7e3b
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-12-11 11:33:34 -0300

    I created connection with 'mysql' database and show data in '/noticias'

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,21 @@
 module.exports = function(app) {
 
-    app.get('/noticias', (req, res) => {
-        res.render("noticias/noticias");
+    app.get('/noticias', (req, res) => {      
+
+        const mysql = require('mysql');
+        const connection = mysql.createConnection({
+            host: "localhost",
+            user: "paulo",
+            password: "123456",
+            database: "portal_noticias"
+        });
+
+        connection.query('select * from noticias', (error, result) => {
+            res.send(result);
+        });  
+
+        //res.render("noticias/noticias");
+        
     });
 
 }
